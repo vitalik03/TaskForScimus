a2e5aaa518ba8d203193f3243c88d996ad5db028
Vitalik <vitalik.vovryk@gmail.com>
2020-02-23 22:23:15 +0200

    Fix bugs

--- routes/users/indexController.js
+++ routes/users/indexController.js
@@ -4,7 +4,7 @@ const getOneUser = async (req, res) => {
     const id = req.params.id;
     const findOne = await new UserService().getOneUser(id);
     if(!findOne){
-        return res.status(400).json({ msg: 'user with this id doesn`t exist'});    
+        return res.status(400).send({ error: 'user with this id doesn`t exist'});    
     }
     return res.status(200).json(findOne);
 }
